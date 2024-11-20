da839a63e9cd178d271d487bab9afca28b8ed606
coniitav <constanza.vergara.villar@gmail.com>
2024-11-19 21:58:35 -0300

    hojas de estilos incluida

--- homepage/views.py
+++ homepage/views.py
@@ -3,5 +3,37 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    context = {}
+    context = {
+        "providers": [
+            {
+                "first_name": "Pepito",
+                "last_name": "Perez",
+                "foto": "https://whoofpoint.com/media/images/card_id/profile_73870520.png"
+            },
+            {
+                "first_name": "Juan",
+                "last_name": "Ramirez"
+            },
+            {
+                "first_name": "María",
+                "last_name": "Suarez"
+            },
+            {
+                "first_name": "Vanessa",
+                "last_name": "Rojas"
+            },
+            {
+                "first_name": "Michael",
+                "last_name": "Torres"
+            },
+            {
+                "first_name": "Ramiro",
+                "last_name": "González"
+            },
+            {
+                "first_name": "Eduardo",
+                "last_name": "Rojas"
+            },
+        ]
+    }
     return render(request, "homepage/index.html", context)
